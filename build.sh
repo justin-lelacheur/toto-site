5dac15bf38451a0677f4d748ca3ccc3db87818f0
Justin Lelacheur <justinlelacheur@Justins-MacBook-Air.local>
2026-09-24 13:43:30 -0400

    Self-host IBM Plex fonts

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 set -eu
 
 zensical build
+
+find site -name '*.html' -print0 | xargs -0 perl -0pi -e 's#\s*<link rel="preconnect" href="https://fonts\.gstatic\.com" crossorigin>\s*##g; s#\s*<link rel="stylesheet" href="https://fonts\.googleapis\.com/[^>]+>\s*##g; s#\s*<style>:root\{--md-text-font:"Inter";--md-code-font:"JetBrains Mono"\}</style>\s*##g'
+
 cp _headers site/_headers
